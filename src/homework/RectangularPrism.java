17dce51351e5af7a261e6d6ce19bb6d216ece0b0
EBRILLO03 <42975610+EBRILLO03@users.noreply.github.com>
2018-10-16 11:26:27 -0400

    Update RectangularPrism.java

--- src/homework/RectangularPrism.java
+++ src/homework/RectangularPrism.java
@@ -8,7 +8,25 @@ public class RectangularPrism {
 	
 	public static void main(String[] args) {
 		 
-		Scanner area = new Scanner(System.in);
+		Scanner scan = newScanner(System.in);
+		System.out.print("Please enter your name:     ");
+		String name;
+		name = scan.nextLine();
+		System.out.println("\n\nHello, " + name + ", welcome to the geometry calculator.");
+		System.out.println("Press enter to continue...");
+		scan.nextLine();
+		System.out.println("AREA OF A TRIANGLE");
+		System.out.print("Please enter the measure of the base");
+		String base8tr =  scan.nextLine();
+		System.out.println("Please enter the measure of the height");
+		String height8tr = scan.nextLine();
+		double base8tr;
+		base = double.parceDouble(base8tr);
+		double height = double.parceDouble(height8tr);
+		System.out.println("Please enter a unit of measurement:    ");
+		String unit = scan.nextLine();
+		double area = .5*(base*height);
+		System.out.println("The area of a triangle is " + area + unit "2");Scanner area = new Scanner(System.in);
 		   System.out.println("Length:   ");
 		   double length = area.nextDouble();
 		   System.out.println("Width:   ");
